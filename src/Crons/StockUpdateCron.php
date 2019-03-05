d44248bfe2c76d51b83c512b293cf144f1d2fd4a
Arun Singh <contact2arunsingh@gmail.com>
2019-03-05 17:48:28 +0530

    Added cron functionality

--- src/Crons/StockUpdateCron.php
+++ src/Crons/StockUpdateCron.php
@@ -9,7 +9,7 @@ class StockUpdateCron extends Cron {
 
 
 	public function handle() {
-		App::call('StockUpdatePlugin\Controllers\ContentController@update_stock');
+		//App::call('StockUpdatePlugin\Controllers\ContentController@update_stock');
 
 	}
 }
